9a6d35ec27b3e9c695a9b0085873006f918b5dbb
Kostoski Stefan <kostoski.stefan90@gmail.com>
2023-04-09 23:21:50 +0200

    Fix lsp encoding and add key mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -2,7 +2,7 @@
 -- more than 1 buffers in the list, otherwise we exit vim
 function _G.buffer_handler()
     local numberOfOpenBuffers = vim.fn.len(vim.fn.getbufinfo({ buflisted = 1 })) 
-    vim.cmd(numberOfOpenBuffers == 1 and 'exit' or 'BufferClose')
+    vim.cmd(numberOfOpenBuffers == 1 and 'exit' or 'bp | bd #')
 end
 
 -- Set leader key
@@ -19,15 +19,23 @@ vim.cmd('cnoreabbrev Wq wq')
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
 vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
 
-vim.keymap.set('n', '<leader>e', '<cmd>NeoTreeFocusToggle<cr>');
-
-vim.keymap.set('n', '<A-h>', function() require('nvterm.terminal').toggle 'horizontal' end)
-vim.keymap.set('t', '<A-h>', function() require('nvterm.terminal').toggle 'horizontal' end)
+vim.keymap.set('n', '<leader>e', '<cmd>NeoTreeFocusToggle<cr>')
 
 vim.keymap.set('n', '<C-_>', '<cmd>lua require("Comment.api").toggle.linewise.current()<cr>')
 vim.keymap.set('v', '<C-_>', '<Esc><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<cr>')
+
+vim.keymap.set('n',         'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+vim.keymap.set('n',         'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+vim.keymap.set('n',         'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+vim.keymap.set('n',  '<leader>f', '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<cr>')
+vim.keymap.set('n', '<leader>fm', '<cmd>lua vim.lsp.buf.format()<cr>')
+
+vim.keymap.set('n', '<A-h>', function() require('nvterm.terminal').toggle 'horizontal' end)
+vim.keymap.set('t', '<A-h>', function() require('nvterm.terminal').toggle 'horizontal' end)
+vim.keymap.set('n', '<F12>', function() require('nvterm.terminal').toggle 'float' end)
+vim.keymap.set('t', '<F12>', function() require('nvterm.terminal').toggle 'float' end)
+
+vim.keymap.set('n', '<Esc>', '<cmd>noh<cr>')
 vim.keymap.set('n', '<C-s>', '<cmd>w<cr>')
 vim.keymap.set('i', '<C-s>', '<Esc>:w<cr>a')
-vim.keymap.set('i', '<C-z>', '<Esc>ua')
 vim.keymap.set('n', '<C-w>', '<cmd>call v:lua.buffer_handler()<cr>')
-vim.keymap.set('n', '<leader>fm', '<cmd>lua vim.lsp.buf.format()<cr>')
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -98,6 +98,8 @@ cmp.setup.cmdline(':', {
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.offsetEncoding = { 'utf-16' }
+
 require('lspconfig').clangd.setup {
-    capabilities = capabilities
+    capabilities = capabilities,
 }
